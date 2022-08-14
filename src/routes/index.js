611bba126f4836c8c36f3a139e857c1790216f09
yoorimChoi <yulim3839@naver.com>
2022-08-15 02:34:02 +0900

    chore: ⚙️ 테스트 코드 정리

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 router.use("/api", require("./api"));
-let func = 
-  function () {
-    let foo = "text";
-return foo;
+
 };
 module.exports = router;
